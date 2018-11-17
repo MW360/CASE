7278f0a6ed6f5b360c58e1eb34223f87682a3d96
iAIex <32507826+iAIex@users.noreply.github.com>
2018-11-17 16:59:44 +0100

    Frontend Episode 2: A New Beginning

---
+++ frontend/deprecated/workflow.js
@@ -0,0 +1,4 @@
+var workflowGrid = new Grid(document.getElementById("workflow-grid"));
+var devicelist = [];
+var blankNode = new Node("Gerät wählen",165,200,[],[])
+devicelist.push(new Node("testnode",150,200,[],[]));
