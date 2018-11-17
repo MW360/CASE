d0b10e0c9c12eef0c2a8e1c8f3663e92a913d6af
iAIex <32507826+iAIex@users.noreply.github.com>
2018-11-17 23:45:37 +0100

    completed basic set of get endpoints

--- backend/restEndpoints.py
+++ backend/restEndpoints.py
@@ -1,6 +1,7 @@
 import flask
 import glob
 import json
+import os
 from flask_cors import CORS, cross_origin
 app = flask.Flask('moin')
 cors = CORS(app)
@@ -9,7 +10,7 @@ cors = CORS(app)
 @cross_origin()
 def getDevices():
     templist = []
-    jsonFiles = glob.glob('./devices/*.json')
+    jsonFiles = glob.glob('devices/*.json')
     for file in jsonFiles:
         with open(file) as json:
             tempcontent = ''
@@ -20,14 +21,18 @@ def getDevices():
 
 @app.route('/devices/<devicename>')
 @cross_origin()
-def api_article(devicename):
-    return devicename
-
+def getDeviceByName(devicename):
+    if os.path.isfile('devices/' + devicename + '.json'):
+        with open('devices/' + devicename + '.json') as json:
+            return json.read()
+    else:
+        return "NOPE", 404
+    
 @app.route('/workflows', methods=['GET'])
 @cross_origin()
 def getWorkflows():
     templist = []
-    jsonFiles = glob.glob('workflow/*.json')
+    jsonFiles = glob.glob('workflows/*.json')
     for file in jsonFiles:
         with open(file) as json:
             tempcontent = ''
@@ -35,3 +40,12 @@ def getWorkflows():
                 tempcontent = (tempcontent + line.strip())
             templist.append(tempcontent)
     return str(templist).replace('\'','')
+
+@app.route('/workflows/<workflowname>')
+@cross_origin()
+def getWorkflowByName(workflowname):
+    if os.path.isfile('workflows/' + workflowname + '.json'):
+        with open('workflows/' + workflowname + '.json') as json:
+            return json.read()
+    else:
+        return "NOPE", 404
