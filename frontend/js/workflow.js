4d8adf2d4cb909549dd3a28d62103643a4814c62
Matthias Griebl <matthias.griebl@tum.de>
2018-11-17 22:18:42 +0100

    Added js and css files

--- frontend/js/workflow.js
+++ frontend/js/workflow.js
@@ -1,2 +1,28 @@
 var workflowGrid = new Grid(document.getElementById("workflow-grid"));
 var nodelist = [];
+
+var graph = new mxGraph(document.getElementById("workflow-grid"));
+graph.setConnectable(true)
+graph.setPortsEnabled(false)
+
+new mxRubberband(graph)
+
+var parent = graph.getDefaultParent()
+
+
+var h = document.getElementById('h')
+var fn = () => {
+    graph.getModel().beginUpdate()
+    //Could be put into function that is called for each node
+    try {
+        let square = graph.insertVertex(parent, null, "X", 10, 10, 100, 50, 'rounded')
+        square.setConnectable(false)
+        graph.insertVertex(square, null, "Y", 0, 0, 10, 10)
+    } finally {
+        graph.getModel().endUpdate()
+    }
+}
+
+
+
+mxUtils.makeDraggable(h, graph, fn, null, 0, 0, true, true)
