121087475abfdd1e524a174133a1a5174814ecaf
BerryBlue2715 <frey.bastian@gmx.de>
2018-11-17 13:37:20 +0100

    Added files

--- backend/DataStruct.py
+++ backend/DataStruct.py
@@ -1,42 +1,53 @@
-import DataClass
+import sys
 import json
+import os
 Workflow = {}
+Devices = {}
+Scripts = {}
+Driver = {}
+
+def GetWorkflows():
+    ls = os.listdir("./Workflows/");
+    for f in ls:
+        if ".json" in f:
+            w = open("./Workflows/"+f);
+            data = w.read();
+            w.close();
+            Workflow[f.split(".")[0]] = json.loads(data);
+
+def GetNodes():
+    ls = os.listdir("./Devices/");
+    for f in ls:
+        if ".json" in f:
+            w = open("./Devices/" + f);
+            data = w.read();
+            w.close();
+            Devices[f.split(".")[0]] = json.loads(data);
+def CreateNewDevice():
+    pass
+
+def GetScript():
+    ls = os.listdir("./Devices/");
+    for f in ls:
+        if ".py" in f:
+            filepath = "Devices/" + f
+
+            directory, fnc_name = os.path.split(filepath)
+
+            path = list(sys.path)
+            sys.path.insert(0, directory)
+
+            try:
+                i = __import__(f.split(".")[0])
+            finally:
+                sys.path[:] = path
+            Scripts[f.split(".")[0]] = i;
 
 
-def GetWorkflow(workflow):
-    data = json.loads(workflow);
 
-
-
-def GenerateNodes():
-    node = DataClass.Node();
-    node.name = "Hallo";
-    node.description = "...";
-    node.inputs = []
-
-    inp = DataClass.Input();
-    inp.name = "Input0";
-    inp.type = "bool";
-    inp.description = "...";
-    node.inputs.Append(inp);
-
-    inp = DataClass.Input();
-    inp.name = "Input1";
-    inp.type = "bool";
-    inp.description = "...";
-    node.inputs.Append(inp);
-
-    out = DataClass.Output();
-
-    
-
-
-
-
-
-    pass;
-
-
-w = open("./venv/Workflow.json", "r");
-r = w.read();
-GetWorkflow(r);
+GetWorkflows();
+GetNodes();
+GetScript();
+print(Workflow.keys());
+print(Devices.keys());
+print(Scripts.keys());
---
+++ backend/Devices/Fnc.py
@@ -0,0 +1,4 @@
+def Do(args = []):
+    print(args);
+    args_ret = []
+    return args_ret;
