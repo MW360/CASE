44ec0f5b01a322c6bd94a91e4542518883075547
iAIex <32507826+iAIex@users.noreply.github.com>
2018-11-17 23:20:56 +0100

    Added CORS to endpoints

--- backend/restEndpoints.py
+++ backend/restEndpoints.py
@@ -1,9 +1,12 @@
 import flask
 import glob
 import json
+from flask_cors import CORS, cross_origin
 app = flask.Flask('moin')
+cors = CORS(app)
 
 @app.route('/devices', methods=['GET'])
+@cross_origin()
 def getDevices():
     templist = []
     jsonFiles = glob.glob('./devices/*.json')
@@ -15,7 +18,13 @@ def getDevices():
             templist.append(tempcontent)
     return str(templist).replace('\'','')
 
+@app.route('/devices/<devicename>')
+@cross_origin()
+def api_article(devicename):
+    return devicename
+
 @app.route('/workflows', methods=['GET'])
+@cross_origin()
 def getWorkflows():
     templist = []
     jsonFiles = glob.glob('workflow/*.json')
